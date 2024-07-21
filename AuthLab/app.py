ce8ad2f7ea53c2d589e32adff83b005ddae50164
alisash24-meet <alisa.shapovalov@gmail.com>
2024-07-21 18:30:25 +0300

    Update1

--- AuthLab/app.py
+++ AuthLab/app.py
@@ -8,11 +8,40 @@ firebaseConfig = {
   "projectId": "authincation-lab",
   "storageBucket": "authincation-lab.appspot.com",
   "messagingSenderId": "724672379450",
-  "appId": "1:724672379450:web:f92a0d6538ccea8fe39303"
+  "appId": "1:724672379450:web:f92a0d6538ccea8fe39303",
+  "databaseURL":""
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder='Templates', static_folder='static')
 
 app.config['SECRET_KEY'] = 'super-secret-key'
+
+@app.route("/",methods= ['POST','GET'])
+def sign_up():
+  if request.method == 'GET':
+    return render_template('signup.html')
+  else:
+    login_session["email"]= request.form("email")
+    login_session["password"]= request.form("password")
+
+
+@app.route("/signin")
+def sign_in():
+  return render_template('signin.html')
+
+@app.route("/home")
+def home():
+  return render_template('home.html')
+
+@app.route("/thanks")
+def thanks():
+  return render_template('thanks.html')
+
+@app.route("/display")
+def display():
+  return render_template('display.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
