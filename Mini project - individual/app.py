48a401b916c49a014823db828a4d94065585f34b
alisash24-meet <alisa.shapovalov@gmail.com>
2024-07-23 18:37:41 +0300

    update2

--- Mini project - individual/app.py
+++ Mini project - individual/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask import session as login_session
 import pyrebase
 import firebase
+import random
 
 
 firebaseConfig = {
@@ -16,13 +17,14 @@ firebaseConfig = {
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
-db= firebase.database()
+db = firebase.database()
 auth = firebase.auth()
-app = Flask(__name__, template_folder='Templates', static_folder='static')
 
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 lana_facts=["Lana's real name is Elizabeth Woolridge Grant.","Lana studied metaphysics in college.","Lana has a tatto of the letter M in honor of her grandmother.","Lana was born on the 21st of June, 1985.","Her first stage name was May Jailer.","Lana has been nominated for 6 Grammy Awards and a Golden Globe Award.","Lana has two apartments, one in New York and the other in L.A. where she lives with her brother and sister."]
+lana_del=[" Pray"," Slay"," Tray"," Neigh"," Clay"," Lay"," Bay"]
 
 @app.route("/", methods=['POST','GET'])
 def home():
@@ -47,21 +49,21 @@ def sign_in():
 def sign_up():
 	if request.method == 'GET':
 		return render_template('signup.html')
-  else:
-    email = request.form["email"]
-    password= request.form["password"]
-    fullname= request.form["fullname"]
-    username= request.form["username"]
-    user = {"password": password, "username": username,"email": email}
-    try:
-      user = {"password": password, "username": username,"email": email}
-      login_session['user']= auth.create_user_with_email_and_password(email, password, username)
-      UID=login_session['user']['localId']
-      db.child("Users").child(UID).set(user)
-      return redirect(url_for('options')) 
-    except:
-      print("Authentication failed :(")
-      return redirect('error')
+	else:
+	    email = request.form["email"]
+	    password= request.form["password"]
+#	    fullname= request.form["fullname"]
+	    username= request.form["username"]
+	    user = {"password": password, "username": username,"email": email}
+	    try:
+	      user = {"password": password, "username": username,"email": email}
+	      login_session['user']= auth.create_user_with_email_and_password(email, password)
+	      UID=login_session['user']['localId']
+	      db.child("Users").child(UID).set(user)
+	      return redirect(url_for('options')) 
+	    except:
+	      print("Authentication failed :(")
+	      return redirect('error')
 
 @app.route("/options", methods=['POST','GET'])
 def options():
@@ -73,8 +75,18 @@ def songs():
 
 @app.route("/random", methods=['POST','GET'])
 def randomized():
-	chosen_facts=lana_facts [random.randint(0, 8)]
-	return render_template('random.html')
+	if request.method == 'POST':
+		button1 = request.form["button"]
+		print(button1)
+		chosen_fact=""
+		chosen_lana=""
+		if button1 =="Random Fact:":
+			chosen_fact=lana_facts [random.randint(0, 6)]
+		else:
+			chosen_lana=lana_del [random.randint(0,6)]
+		return render_template('random.html',chosen_fact2 = chosen_fact, chosen_lana2 = chosen_lana)
+	else:
+		return render_template('random.html')
 
 @app.route('/signout')
 def signout():
@@ -95,7 +107,7 @@ def bio():
 		quote = request.form["quote"]
 		input1.append(quote)
 		user["quote"]=quote
-  		db.child("Users").child[UID].set(user)
+		db.child("Users").child(UID).set(user)
 
 
 @app.route('/error', methods=['POST','GET'])
